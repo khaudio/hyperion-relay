2d31c70f8856803f528d13b2f1a2a518913133ec
Kyle Hughes <kylehughes1@gmail.com>
2018-03-08 02:06:29 -0600

    updated docstrings and fixed status LED illumination

--- hyperion_relay.py
+++ hyperion_relay.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-
+Activate a relay breakout board when color data is sent to hyperion.
+This can be used to control incandescent lights or any non-LED device
+using hyperion.
 """
 
 from re import search
@@ -15,7 +17,9 @@ relayPin, ledPower, ledPin = 2, 21, 3
 
 
 def setup():
-    """Set up GPIO and illuminate the status LED"""
+    """
+    Set up GPIO and illuminate the status LED
+    """
     GPIO.setwarnings(False)
 
     # Use BCM pin numbering
@@ -24,46 +28,50 @@ def setup():
     # Set GPIO pins as outputs
     for pin in (relayPin, ledPower, ledPin):
         GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, 1)
 
 
 def run():
+    """
+    Check hyperion for color information via a subprocess call, then
+    activate the relay if data is found.
+
+    Pulling the relay pin low activates it, while pulling high deactivates it.
+    """
     on = False
 
     while True:
         status = Popen(['hyperion-remote', '-l'], stdout=PIPE).stdout.read().decode('utf-8')
         signal = search('(?<=HEX\sValue"\s:\s\[\s").+(?="\s],)', status)
 
-        print(signal)
         if signal and not on:
-            on = switch_relay(0)
+            on = switch_relay(0) # Activate the relay
 
         elif not signal and on:
-            on = switch_relay(1)
+            on = switch_relay(1) # Deactivate the relay
 
         sleep(.01) # Sleep briefly to lower CPU usage
 
 
 def switch_relay(state):
+    """
+    Switch the relay based on the input command, then return the changed state.
+    """
     GPIO.output(relayPin, state)
     print('Lights {}'.format('off' if state else 'on'))
     return not state
 
 
-def main():
+if __name__ == '__main__':
     setup()
 
     # Flash the status LED twice to verify that the script is running
-    for state in (GPIO.LOW, GPIO.HIGH) * 2:
+    for state in (0, 1) * 2:
         sleep(.25)
         GPIO.output(ledPin, state)
 
-    try: 
+    try:
         run() # Loop to check for color data and process it
 
     finally:
         GPIO.cleanup() # Clean up GPIO
-
-
-if __name__ == '__main__':
-    main()
--- relay_off.py
+++ relay_off.py
@@ -4,7 +4,8 @@
 Deactivate the relay, then exit.  This script will not wait for new commands.
 """
 
-from hyperion_relay import setup
+from hyperion_relay import setup, switch_relay
 
 
 setup()
+switch_relay(1)

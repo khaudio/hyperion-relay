d13189ce341a1cd46d87674d988422af6bd7f3a4
Kyle Hughes <kylehughes1@gmail.com>
2018-03-08 12:49:30 -0600

    cpp works now and added binary

--- hyperion_relay.cpp
+++ hyperion_relay.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <thread>
+#include <chrono>
 #include "subprocess.hpp"
 
 using namespace std;
 
 int main()
 {
-    subprocess::popen proc("hyperion-remote", {"-l"});
-    string status(static_cast<stringstream const&>(stringstream() << proc.stdout().rdbuf()).str());
-
-    std::regex h("\"HEX Value\" : .+,");
+    string status, color;
+    bool on, last;
     std::smatch val;
+    std::regex h("\"HEX Value\" : \\[ \"(.+)\"");
 
-    std::regex_search(status, val, h);
-    for (size_t i = 0; i < val.size(); ++i)
-        cout << val[i] << endl;
+    while (true)
+    {
+        subprocess::popen proc("hyperion-remote", {"-l"});
+        status = (static_cast < stringstream const & > (stringstream() << proc.stdout().rdbuf()).str());
 
+        std::regex_search(status, val, h);
+        color = val[1];
+        on = !color.empty();
+
+	if (on != last)
+        {
+            cout << "Lights " << ((on) ? "on" : "off") << endl;
+        }
+
+	last = on;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
     return 0;
 }

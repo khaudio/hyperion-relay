bd8df846000a3fc8c4d35e986a595b45493c9d8c
Kyle Hughes <kylehughes1@gmail.com>
2018-03-07 19:14:41 -0600

    reworked loop and removed unnecessary files

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 
-apt update &&
+apt update
 apt install python3-rpi.gpio pyserial
 
-./install_hyperion.sh
+curl -k -L --output install_hyperion.sh --get https://raw.githubusercontent.com/hyperion-project/hyperion/master/bin/install_hyperion.sh
+
+chmod +x install_hyperion.sh
+sh ./install_hyperion.sh
+
 service hyperion stop
 
-cp ./default_on /etc/hyperion.config.json
+cp ./hyperion.config.json /etc/hyperion/
+
+chmod +x ./hyperion-relay
+cp ./hyperion-relay /etc/hyperion/
+
+cat >> /etc/systemd/system/hyperion-relay.service << EOF
+[Unit]
+Description=hyperion-relay
+
+[Service]
+ExecStart=/etc/hyperion/hyperion-relay.py
+StandardOutput=tty
+StandardError=syslog
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl daemon-reload
+
 service hyperion start
+service hyperion-relay start

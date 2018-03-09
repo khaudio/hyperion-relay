1e55d5a9575f6d45f09907b5f418d4a609057ef0
Kyle Hughes <kylehughes1@gmail.com>
2018-03-08 19:27:23 -0600

    cpp version works with gpio/relay and added compile.sh because i dont know how to use cmake or whatever

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ -Wall --std=c++11 hyperion_relay.cpp -lwiringPi -o hyperion_relay
--- hyperion_relay.cpp
+++ hyperion_relay.cpp
@@ -3,12 +3,23 @@
 #include <regex>
 #include <thread>
 #include <chrono>
+#include <wiringPi.h>
 #include "subprocess.hpp"
 
 using namespace std;
 
 int main()
 {
+    int relay = 2;
+    wiringPiSetupGpio();
+    pinMode(2, OUTPUT);
+    pinMode(21, OUTPUT);
+    pinMode(3, OUTPUT);
+
+    digitalWrite(21, 1);
+    digitalWrite(3, 1);
+    digitalWrite(2, 1); 
+
     string status, color;
     bool on, last;
     std::smatch val;
@@ -26,6 +37,7 @@ int main()
 	if (on != last)
         {
             cout << "Lights " << ((on) ? "on" : "off") << endl;
+            digitalWrite(2, ((on) ? LOW : HIGH));
         }
 
 	last = on;
